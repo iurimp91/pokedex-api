32495411ad72d41599dd4ba745819da8d2df1bc7
Iuri Magnago <iurimp91@gmail.com>
2021-08-06 20:25:46 -0300

    Populate Heroku DB

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,10 @@ import * as pokemonController from "./controllers/pokemonController";
 import checkToken from "./middlewares/checkToken";
 import SignUpParamsValidation from "./middlewares/SignUpParamsValidation";
 
+import axios from "axios";
+import { getRepository } from "typeorm";
+import Pokemon from "./entities/Pokemon";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -25,6 +29,29 @@ app.get("/pokemons", checkToken, pokemonController.getPokemons);
 app.post("/my-pokemons/:id/add", checkToken, pokemonController.catchPokemon)
 
 app.post("/my-pokemons/:id/remove", checkToken, pokemonController.releasePokemon)
+
+app.post("/populate", async (req,res)=>{
+ 
+  for(let i = 1; i < 152; i ++){
+    const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    const newPokemon = {
+      id: result.data.id,
+      name: result.data.name,
+      number: result.data.id,
+      image: result.data.sprites.front_default,
+      weight: result.data.weight,
+      height: result.data.height,
+      baseExp: result.data.base_experience,
+      description: ""
+    }
+ 
+    const speciesResult = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i}`)
+    newPokemon.description = speciesResult.data.flavor_text_entries[0].flavor_text.split("\n").join(" ")
+    const pokemon = getRepository(Pokemon).create(newPokemon)
+    const resultquery = await getRepository(Pokemon).save(pokemon)
+  }
+  res.send("OK")
+})
 
 app.use(async (err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err.message);
